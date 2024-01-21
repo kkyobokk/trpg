8b31af73ff5d850fb2c36e42047c8a4f776e14e5
송은빈 <trpgtravel@gmail.com>
2024-01-21 23:10:33 +0900

    err

--- Downloads/trpg-main/Item/Inventory/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['inv','arm_inv']
--- Downloads/trpg-main/Item/Inventory/test.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-import tkinter.ttk as ttk
-root = Tk()
-root.geometry("500x500")
-
-log_list = Text(root,width=50,height=34, relief="solid",background="white", font=("Aria",12),bd=0,wrap="word")
-log_list.place(x=0,y=0)
-log_list.insert(0.0,"게임에 접속했습니다.")
-root.mainloop()
